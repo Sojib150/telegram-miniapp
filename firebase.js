6abfdf145ee718b8f74fe974857ef7e889fb0296
Sojib Ahmed <sojiba1500@gmail.com>
2025-09-26 00:48:25 +0800

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,10 @@
+// firebase.js
+// keep as non-module (or module) and import where needed
+export const firebaseConfig = {
+  apiKey: "AIzaSyB6b93A_HeU4FADs3o2Ysw6-dlRRS2TbZk",
+  authDomain: "telegram-miniapp-e8cc0.firebaseapp.com",
+  projectId: "telegram-miniapp-e8cc0",
+  storageBucket: "telegram-miniapp-e8cc0.firebasestorage.app",
+  messagingSenderId: "827930913054",
+  appId: "1:827930913054:web:502b56e0c198d8e9ff410f"
+};
